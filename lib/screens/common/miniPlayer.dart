1852c809134fd84633a5fcb3d81b1ffabf7c71c7
Micael <up202303869@up.pt>
2026-09-13 19:01:14 +0100

    mini player on tap navigate to song page

--- lib/screens/common/miniPlayer.dart
+++ lib/screens/common/miniPlayer.dart
@@ -15,37 +15,27 @@ import 'songSlider.dart';
 class MiniPlayerSheet extends StatelessWidget {
   const MiniPlayerSheet({super.key});
 
+  void _onTapNavigateToSongPage(BuildContext context, Song song) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SongPage(song: song))
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<SongPlayer>(
       builder: (context, songPlayer, child) {
+
         if (songPlayer.playingSong == null) {
           return const SizedBox.shrink();
         }
 
         Song song = songPlayer.playingSong!;
 
-        return Stack(
-          children: [
-            Positioned.fill(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: ImageFiltered(
-                  imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                  child: Transform.scale(
-                    scale: 1, 
-                    child: song.imageWidget,
-                  ),
-                ),
-              ),
-            ),
-
-            GlassWidget(
-              blurSigma: 8,
-              cornerRadius: 20,
-              child: MiniPlayerContents(songPlayer: songPlayer),
-            ),
-          ],
+        return GestureDetector(
+          onTap: () => _onTapNavigateToSongPage(context, song),
+          child: _MiniPlayerWidget(songPlayer: songPlayer, song: song)
         );
       },
     );
@@ -53,9 +43,44 @@ class MiniPlayerSheet extends StatelessWidget {
 }
 
 
-class MiniPlayerContents extends StatelessWidget {
+class _MiniPlayerWidget extends StatelessWidget {
 
-  const MiniPlayerContents({super.key, required this._songPlayer});
+  const _MiniPlayerWidget({super.key, required this.songPlayer, required this.song});
+
+  final SongPlayer songPlayer;
+  final Song song;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+              child: Transform.scale(
+                scale: 1, 
+                child: song.imageWidget,
+              ),
+            ),
+          ),
+        ),
+    
+        GlassWidget(
+          blurSigma: 8,
+          cornerRadius: 20,
+          child: _MiniPlayerContents(songPlayer: songPlayer),
+        ),
+      ],
+    );
+  }
+}
+
+
+class _MiniPlayerContents extends StatelessWidget {
+
+  const _MiniPlayerContents({super.key, required this._songPlayer});
 
   final SongPlayer _songPlayer;
 
